d33f14aebcd123d626ca7f273fc0814026ed5127
grandtheft-spacehip <justdizon71@gmail.com>
2016-11-24 00:32:39 -0800

    :art: Stackoverflow Parser tutorial

---
+++ spec/crumbfetch-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CrumbfetchView from '../lib/crumbfetch-view';
+
+describe('CrumbfetchView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
